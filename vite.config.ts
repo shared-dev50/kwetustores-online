5296f559b34b6256f71f395c9e40c2fcf13d3ad9
MatoG50 <matog50@gmail.com>
2026-03-30 20:51:04 +0300

    route change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/kwetustores-online",
   server: {
     proxy: {
       "/api": {
